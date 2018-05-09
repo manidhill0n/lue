d27452d93b6d2b69c7f0693a7a06df9bbe6afc4f
manidhill0n <manidhillon1989@gmail.com>
2018-05-09 14:57:02 +0200

    Add files via upload

---
+++ par1.r
@@ -0,0 +1,8 @@
+#' @title Photosynthetically Active Radiation
+#' @docType data
+#' @name par1
+#' @usage par1
+#' @format A rasterbrick (.nc)
+#' @description Input par dataset
+#' @keywords datasets
+NULL
---
+++ tmin.r
@@ -0,0 +1,8 @@
+#' @title Minimum temperature data
+#' @docType data
+#' @name tmin
+#' @usage tmin
+#' @format A raster (.nc)
+#' @description Input minimum temperature dataset
+#' @keywords datasets
+NULL
